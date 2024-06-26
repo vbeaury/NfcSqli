e2ec46ff773490a62e3d9ca870bfac25a54a5f71
Valentin Beaury <vbeaury@sqli.com>
2024-06-26 16:50:17 +0200

    Init project

---
+++ NfcSqli/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NfcSqli;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ NfcSqli/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NfcSqli;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
